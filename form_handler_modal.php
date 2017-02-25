fc9f6259c49d1c7816a4969f1fc5c5faac8c0941
oxyyyyy <elektronikaion@gmail.com>
2017-02-25 17:51:41 +0200

    Added footer contacts section handler

---
+++ form_handler_modal.php
@@ -0,0 +1,32 @@
+<?php
+
+/* Задаем переменные */
+
+$name = htmlspecialchars($_POST["name726"]);
+$email = htmlspecialchars($_POST["email264"]);
+$message = htmlspecialchars($_POST["message294"]);
+$mail = "info@studio777.com";
+
+/* Ваш адрес и тема сообщения */
+
+$address = "elektronikaion@gmail.com";
+$sub = "Новая заявка на сайте a-vlasenko.zzz.com.ua";
+
+/* Формат письма */
+
+$mes = "Поступила новая заявка на сайте a-vlasenko.zzz.com.ua.\n
+Страница: http://a-vlasenko.zzz.com.ua/
+Клиента зовут: $name
+E-mail клиента: $email
+Сообщение клиента: $message";
+
+/* Отправляем сообщение, используя mail() функцию */
+
+$from = "From: $name <$mail> \r\n Reply-To: $mail \r\n";
+if (mail($address, $sub, $mes, $from)) {
+                header('Refresh:1; URL=http://a-vlasenko.zzz.com.ua/');
+                echo 'Письмо отправлено, через секунду вы вернетесь на сайт';}
+else {
+                header('Refresh:1; URL=http://a-vlasenko.zzz.com.ua/');
+                echo 'Письмо почему-то не отправлено, через секунду Вы вернетесь на сайт';}
+?>
