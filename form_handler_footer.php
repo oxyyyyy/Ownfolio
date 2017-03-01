ad9bba0cbc89c11b714ae73f1165e24e3558df95
oxyyyyy <elektronikaion@gmail.com>
2017-03-01 16:26:55 +0200

    Footer contact form is working with ajax!

--- form_handler_footer.php
+++ form_handler_footer.php
@@ -1,51 +1,30 @@
 <?php
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["message"]);
+  /* Задаем переменные */
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-            exit;
-        }
+  $name = htmlspecialchars($_POST["name"]);
+  $email = htmlspecialchars($_POST["email"]);
+  $message = htmlspecialchars($_POST["message"]);
+  $mail = "info@a-vlasenko.zzz.com.ua";
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "info@a-vlasenko.zzz.com.ua";
+  /* Ваш адрес и тема сообщения */
 
-        // Set the email subject.
-        $subject = "New contact from $name";
+  $address = "info@a-vlasenko.zzz.com.ua";
+  $sub = "Новая заявка на сайте a-vlasenko.zzz.com.ua";
 
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
+  /* Формат письма */
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+  $mes = "Поступила новая заявка на сайте a-vlasenko.zzz.com.ua.\n
+  Страница: http://a-vlasenko.zzz.com.ua/
+  Клиента зовут: $name
+  E-mail клиента: $email
+  Сообщение клиента: $message";
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
+  /* Отправляем сообщение, используя mail() функцию */
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
-
+  $from = "From: $name <$mail> \r\n Reply-To: $mail \r\n";
+  if (mail($address, $sub, $mes, $from)) {
+                  echo 'Sent!';}
+  else {
+                  echo 'Error.';}
 ?>
